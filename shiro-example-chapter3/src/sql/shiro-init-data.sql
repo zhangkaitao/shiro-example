9cce13dd09fc7a8ae8da867f89de63cd6a03909e
zhangkaitao <zhangkaitao0503@gmail.com>
2014-01-26 22:05:40 +0800

    shiro教程

---
+++ shiro-example-chapter3/src/sql/shiro-init-data.sql
@@ -0,0 +1,10 @@
+delete from users;
+delete from user_roles;
+delete from roles_permissions;
+insert into users(username, password, password_salt) values('zhang', '123', null);
+insert into user_roles(username, role_name) values('zhang', 'role1');
+insert into user_roles(username, role_name) values('zhang', 'role2');
+insert into roles_permissions(role_name, permission) values('role1', '+user1+10');
+insert into roles_permissions(role_name, permission) values('role1', 'user1:*');
+insert into roles_permissions(role_name, permission) values('role1', '+user2+10');
+insert into roles_permissions(role_name, permission) values('role1', 'user2:*');
