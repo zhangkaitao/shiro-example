8a953f7e05c1314ca03719a32c9b1f9b61c9ad16
zhangkaitao <zhangkaitao0503@gmail.com>
2014-02-14 14:41:45 +0800

    shiro教程-第十四章

---
+++ shiro-example-chapter15-client/src/main/java/com/github/zhangkaitao/shiro/chapter13/service/PermissionServiceImpl.java
@@ -0,0 +1,26 @@
+package com.github.zhangkaitao.shiro.chapter13.service;
+
+import com.github.zhangkaitao.shiro.chapter13.dao.PermissionDao;
+import com.github.zhangkaitao.shiro.chapter13.entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public class PermissionServiceImpl implements PermissionService {
+
+    private PermissionDao permissionDao;
+
+    public void setPermissionDao(PermissionDao permissionDao) {
+        this.permissionDao = permissionDao;
+    }
+
+    public Permission createPermission(Permission permission) {
+        return permissionDao.createPermission(permission);
+    }
+
+    public void deletePermission(Long permissionId) {
+        permissionDao.deletePermission(permissionId);
+    }
+}
---
+++ shiro-example-chapter15-client/src/main/webapp/casFailure.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+     CAS失败了，如错误的Ticket或证书错误等。
+</body>
+</html>
